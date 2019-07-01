050d4d45d0822199686076acd797c0ec00a3489f
JunioJsv <juniojsv@gmail.com>
2019-06-30 21:11:31 -0300

    Various UI and UX enhancements

--- app/src/main/java/juniojsv/mediatekeasyroot/MainActivity.kt
+++ app/src/main/java/juniojsv/mediatekeasyroot/MainActivity.kt
@@ -1,5 +1,7 @@
 package juniojsv.mediatekeasyroot
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,6 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.elevation = 0.0F
+
+        version_text.text = "Version ${BuildConfig.VERSION_NAME}"
+
+        button_donate.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(
+                    "https://www.mercadopago.com/mlb/checkout/start?pref_id=365594257-359f7b8e-cc7c-4ff2-8fd1-4fc73eb6de50"
+                )
+            })
+        }
+
+        button_github.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(
+                    "https://github.com/JunioJsv/mediatek-easy-root"
+                )
+            })
+        }
 
         button_try_root.setOnClickListener {
             TryRoot(this, this).execute()
