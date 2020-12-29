2bfcfcbbff42f9e335ddc0898c74871d3acf2446
Jeovane Santos <juniojsv@gmail.com>
2020-12-29 15:53:44 -0300

    Organizing the project - fix mistake on Coroutine dependency - the ThreadTask class does not work, i implement the CoroutineTask - I did a little refactoring in the code - it is now possible to ignore the security patch warning

---
+++ app/src/main/java/juniojsv/mtk/easy/su/Keys.kt
@@ -0,0 +1,8 @@
+package juniojsv.mtk.easy.su
+
+const val PREF_SECURITY_PATCH_IGNORED = "security_patch_warning_ignored"
+const val PREF_STARTUP_WARNING = "startup_warning"
+const val PREF_RUN_AS_64_BITS = "run_as_64"
+const val PREF_EXTRACTED_BY_VERSION = "extracted_by_version"
+const val LOG_ADMOB_INITIALIZATION = "admob_initialization"
+const val LOG_VERITY_UPDATE = "verity_update"
